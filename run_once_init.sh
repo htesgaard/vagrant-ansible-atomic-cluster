610e210eb704c891a0ab8557855c2912eab0bd6f
htesgaard <helge.tesgaard@gmail.com>
2017-02-22 13:09:50 +0100

    Changed file to unix format

--- run_once_init.sh
+++ run_once_init.sh
@@ -4,7 +4,7 @@ ansible-playbook /vagrant/ansible/playbooks/enable_host_only_network_after_reboo
 if [ $? -eq 0 ] ; then
   rm /vagrant/run_once_init.sh
   echo "one-shot provisioning completed"
-
 else
   echo "one-shot provisioning failed"
 fi
+
