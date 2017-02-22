7008e2385eb26d36e9829cb43ba5661fe855aece
htesgaard <helge.tesgaard@gmail.com>
2017-02-22 14:05:30 +0100

    Updated readme

--- run_once_init.sh
+++ run_once_init.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-echo "one-shot provisioning start"
+echo one-shot provisioning start
 ansible-playbook /vagrant/ansible/playbooks/enable_host_only_network_after_reboot.yml
 if [ $? -eq 0 ] ; then
   ansible localhost -m lineinfile -a 'dest=/home/vagrant/.bashrc state=absent regexp=run_once_init.sh'
-  echo "one-shot provisioning completed"
+  echo one-shot provisioning completed
 else
-  echo "one-shot provisioning failed"
+  echo one-shot provisioning failed
 fi
 
