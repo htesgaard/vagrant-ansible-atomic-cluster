c4be4093149306439839d3ce004d537bc4f42054
Helge Tesgaard <helge.tesgaard@gmail.com>
2017-02-22 10:29:42 +0100

    project improvements

--- ansible/config/run_once_init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "Run once provisioning start"
-ansible-playbook /vagrant/ansible/playbooks/enable_host_only_network_after_reboot.yml
-if [ $? -eq 0 ] ; then
-     rm ~/run_once_init.sh
-    echo "Run once provisioning sucessfull"
-else
-    echo "Run once provisioning failed"
-fi
