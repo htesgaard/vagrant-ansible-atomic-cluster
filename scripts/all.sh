9d18a4d530e9f1199cd8c370c10be3e564861763
Helge Tesgaard <helge.tesgaard@gmail.com>
2017-03-28 01:23:28 +0200

    Fixed bugs

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 cd /vagrant/scripts/
 ./prepare_nodes.sh
 ./install_k8s.sh
--- scripts/install_k8s.sh
+++ scripts/install_k8s.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -e
 cd /vagrant/ansible/playbooks/k8s-install/
 for i in *.yml; do ansible-playbook "$i" -i ../VAGRANT_INVENTORY; done
